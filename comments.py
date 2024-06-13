f384ad04bdd307ecd557344055064fb94dc828c0
vincenttommi <vincenttommi@gmail.com>
2024-06-13 15:40:46 +0300

    restategizing examples

---
+++ comments.py
@@ -0,0 +1,5 @@
+x = 50 
+y = 60 
+
+z = x + y
+print('sum', z)
---
+++ keyword_1.py
@@ -0,0 +1,11 @@
+# import  keyword
+# print(keyword.kwlist)
+
+# print(help('range'))
+
+
+# x =  25
+# y  =  20
+
+# z =  y > x
+# print(z)
